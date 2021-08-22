fd1ed5f1a6b4ec7d876e543126bca66a1d175a3f
cuihuawei <huawei.cui@chino-e.com>
2021-08-22 16:02:58 +0800

    REQ-11: Add login activity.
    Description:
    Login Activity,  Using the name and password to login the site.
    
    If login successfully, server will return a session string. keep it.
    
    ST-result: PASS

---
+++ app/StuffManage/app/src/main/java/com/stuff/manage/data/Constant.java
@@ -0,0 +1,14 @@
+package com.stuff.manage.data;
+
+public class Constant {
+
+    public final static String SERVER_ADDRESS ="192.168.2.43";
+
+    // mmmmanage/chklogin_mm.asp?uid=wangbw&password=mmm123
+    // {"ret":"error","message":"Please input correctly username and password."}
+    public final static String LOGIN_ =SERVER_ADDRESS + "/mmmmanage/chklogin_mm.asp";
+
+
+
+
+}
--- app/StuffManage/app/src/main/java/com/stuff/manage/data/LoginDataSource.java
+++ app/StuffManage/app/src/main/java/com/stuff/manage/data/LoginDataSource.java
@@ -13,10 +13,13 @@ public class LoginDataSource {
 
         try {
             // TODO: handle loggedInUser authentication
+
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
                             "Jane Doe");
+
+
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
